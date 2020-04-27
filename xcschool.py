27f0af9c99137e77a9bd0a7870180be22f2d3ef3
Will Wu <willswu@outlook.com>
2020-04-27 11:27:20 +0800

    add isLogin function

--- xcschool.py
+++ xcschool.py
@@ -6,7 +6,7 @@ import http.cookiejar as cookielib
 # session代表某一次连接
 session = requests.session()
 # 因为原始的session.cookies 没有save()方法，所以需要用到cookielib中的方法LWPCookieJar，这个类实例化的cookie对象，就可以直接调用save方法。
-session.cookies = cookielib.LWPCookieJar(filename = "xc.cookies")
+session.cookies = cookielib.LWPCookieJar(filename="xc.cookies")
 
 userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64"
 header = {
@@ -14,6 +14,7 @@ header = {
     "Referer": "https://xcmicro.17zuoye.com/pc/login.html",
     'User-Agent': userAgent,
 }
+
 
 def xcLogin(account, password):
     # 模仿登录
@@ -25,7 +26,7 @@ def xcLogin(account, password):
         "password": password,
     }
     # 使用session直接post请求
-    responseRes = session.post(postUrl, data = postData, headers = header)
+    responseRes = session.post(postUrl, data=postData, headers=header)
     # 无论是否登录成功，状态码一般都是 statusCode = 200
     print(f"statusCode = {responseRes.status_code}")
     print(f"text = {responseRes.text}")
@@ -33,6 +34,28 @@ def xcLogin(account, password):
     session.cookies.save()
 
 
+def isLogin():
+    # 通过访问个人中心页面的返回状态码来判断是否为登录状态
+
+    routeUrl = "https://xcmicro.17zuoye.com/pc/index.html"
+    # 下面有两个关键点
+    # 第一个是header，如果不设置，会返回500的错误
+    # 第二个是allow_redirects，如果不设置，session访问时，服务器返回302，
+    # 然后session会自动重定向到登录页面，获取到登录页面之后，变成200的状态码
+    # allow_redirects = False  就是不允许重定向
+    responseRes = session.get(routeUrl, headers=header, allow_redirects=False)
+    print(f"isLoginStatus = {responseRes.status_code}")
+    if responseRes.status_code != 200:
+        return False
+    else:
+        return True
+
+
 if __name__ == "__main__":
-    # 从返回结果来看，有登录成功
-    xcLogin("xc062040074", "184385")
+    session.cookies.load()
+    if not isLogin():
+        # 从返回结果来看，有登录成功
+        xcLogin("xc062040074", "184385")
+    if isLogin:
+        resp = session.get("https://xcmicro.17zuoye.com/pc/index.html",
+                           headers=header, allow_redirects=False)
