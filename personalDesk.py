503996df65c69bc0f7aa9044c79d6e2b304d577c
Will Wu <willswu@outlook.com>
2020-04-29 03:15:46 +0800

    using 7z unzip

--- personalDesk.py
+++ personalDesk.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import time
 import zipfile
+from unzipmbcs import extractZip
 import requests
 import os
 from bdschool import bdschoolSeminars
@@ -22,9 +23,15 @@ for item in dbs.values():
     fileName = 'files/'+fileName
     with open(fileName, 'wb') as f:
         f.write(res.content)
-    if zipfile.is_zipfile(fileName):
-        with zipfile.ZipFile(fileName,'r') as zf:
-            zf.extractall('files/')
+    
+    os.system('7z x -ofiles "'+fileName+'"')
+    # if zipfile.is_zipfile(fileName):
+    #     if item.subject == '英语':
+    #         extractZip(fileName,'utf-8')
+    #     else:
+    #         with zipfile.ZipFile(fileName,'r') as zf:
+    #             zf.extractall('files/')
+
 #Make a personal portal contains all seminars and student task sheet links from bdschool and xcschool
 
 #raise up web browser to open the portal page
